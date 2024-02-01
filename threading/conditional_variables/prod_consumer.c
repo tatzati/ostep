08de3ae00f0d6ec51e75e5ecc1d801a7c7fec408
Gerardo Tatzati <gerardotatzati@gmail.com>
2024-02-01 10:46:55 -0500

    Create prod_consumer.c

---
+++ threading/conditional_variables/prod_consumer.c
@@ -0,0 +1,30 @@
+int buffer;
+int count = 0;
+
+void put(int value) {
+    assert(count == 0);
+    count = 1;
+    buffer = value;
+}
+
+int get() {
+    assert(count == 1);
+    count = 0;
+    return buffer;
+}
+
+void *producer(void *arg) {
+    int i;
+    int loops = (int) arg;
+    for (i = 0; i < loops; i++) {
+        put(i);
+    }
+}
+
+void *consumer(void *arg) {
+    while (1) {
+        int tmp = get();
+        printf("%d\n", tmp);
+    }
+}
+
